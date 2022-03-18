9100d72c2944acfe024345cd248f87daf03950f8
hyemin <hmwoo94@gmail.com>
2022-03-18 17:06:00 +0900

    [Threeroom] Timer에서 시간 재서 요청 보내기

--- src/elements/Timer.jsx
+++ src/elements/Timer.jsx
@@ -1,45 +1,75 @@
-import React, { useEffect, useRef, useState, useCallback } from 'react';
+import React, { useEffect, useRef, useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreator as gameActions } from "../redux/modules/game";
 
-const Timer = ({ setGameTime }) => {
-    const [renderedStreamDuration, setRenderedStreamDuration] =
-            useState('00:30:00'),
-        streamDuration = useRef(0),
-        previousTime = useRef(0),
-        requestAnimationFrameId = useRef(null),
-        [isStartTimer, setIsStartTimer] = useState(false);
+const Timer = ({ setGameTime, gameEnd }) => {
+  const dispatch = useDispatch();
 
-    const updateTimer = useCallback(() => {
-        let now = performance.now();
-        let dt = now - previousTime.current;
+  const roomId = useSelector((state) => state.room.roomInfo.roomId);
 
-        if (dt >= 1000) {
-            streamDuration.current =
-                streamDuration.current - Math.round(dt / 1000);
-            const formattedStreamDuration = new Date(
-                streamDuration.current * 1000
-            )
-                .toISOString()
-                .substr(11, 8);
-            setRenderedStreamDuration(formattedStreamDuration);
-            previousTime.current = now;
-        }
-        requestAnimationFrameId.current = requestAnimationFrame(updateTimer);
-    }, []);
+  const [renderedStreamDuration, setRenderedStreamDuration] =
+      useState("00:30:00"),
+    streamDuration = useRef(0),
+    previousTime = useRef(0),
+    requestAnimationFrameId = useRef(null),
+    [isStartTimer, setIsStartTimer] = useState(false);
 
-    const startTimer = useCallback(() => {
-        previousTime.current = performance.now();
-        requestAnimationFrameId.current = requestAnimationFrame(updateTimer);
-    }, [updateTimer]);
+  const updateTimer = useCallback(() => {
+    let now = performance.now();
+    let dt = now - previousTime.current;
 
-    useEffect(() => {
-        startTimer();
-    }, []);
+    if (dt >= 1000) {
+      // 1초에 한번씩 stream duration -> 몇 초 지났는지, Math.round로 소수점 보정해줌
+      streamDuration.current = streamDuration.current - Math.round(dt / 1000);
+      const formattedStreamDuration = new Date(streamDuration.current * 1000)
+        .toISOString()
+        .substr(11, 8);
+      // format 형식 맞춰서 바꿔준다.
+      setRenderedStreamDuration(formattedStreamDuration);
+      previousTime.current = now;
+    }
+    // 타이머를 하나씩 맞춰준다.
+    requestAnimationFrameId.current = requestAnimationFrame(updateTimer);
+  }, []);
 
-    return (
-        <div className="timer-controller-wrapper">
-            <div className="timer-display">{renderedStreamDuration}</div>
-        </div>
-    );
+  const startTimer = useCallback(() => {
+    previousTime.current = performance.now();
+    requestAnimationFrameId.current = requestAnimationFrame(updateTimer);
+  }, [updateTimer]);
+
+  useEffect(() => {
+    startTimer();
+  }, []);
+
+  useEffect(() => {
+    if (!gameEnd) return;
+    console.log("Timer.jsx에서 dispatch");
+    const _temp = Math.abs(streamDuration.current);
+
+    //초를 시 분 초로 변경
+    function sec2time(timeInSeconds) {
+      var pad = function (num, size) {
+          return ("000" + num).slice(size * -1);
+        },
+        time = parseFloat(timeInSeconds).toFixed(3),
+        hours = Math.floor(time / 60 / 60),
+        minutes = Math.floor(time / 60) % 60,
+        seconds = Math.floor(time - minutes * 60),
+
+      return pad(hours, 2) + ":" + pad(minutes, 2) + ":" + pad(seconds, 2);
+    }
+
+    var _value = sec2time(_temp);
+
+    dispatch(gameActions.deleteGame(roomId, _value));
+    // dispatch(rankActions.recordTime();
+  }, [gameEnd]);
+
+  return (
+    <div className="timer-controller-wrapper">
+      <div className="timer-display">{renderedStreamDuration}</div>
+    </div>
+  );
 };
 
 export default Timer;
--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -1,14 +1,14 @@
-import { createAction, handleActions } from 'redux-actions';
-import produce from 'immer';
+import { createAction, handleActions } from "redux-actions";
+import produce from "immer";
 
-import instance from '../../shared/request';
+import instance from "../../shared/request";
 
 // actions
-const IS_IN = 'IS_IN';
+const IS_IN = "IS_IN";
 
 // Action Creators
-const setIsIn = createAction(IS_IN, isIn => ({
-    isIn,
+const setIsIn = createAction(IS_IN, (isIn) => ({
+  isIn,
 }));
 
 // initialState
@@ -17,51 +17,50 @@ const initialState = { isIn: false, isCreator: false, peers: [] };
 //  middleware Actions
 
 // 유저 정보 조회하기
-const refUser = roomId => {
-    return function (dispatch, getState) {
-        instance
-            .get(`/user/${roomId}`)
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    };
+const refUser = (roomId) => {
+  return function (dispatch, getState) {
+    instance
+      .get(`/user/${roomId}`)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
 };
 
 // 유저 삭제하기
-const deleteUser = socketId => {
-    return function (dispatch, getState) {
-        console.log('유저삭제', socketId);
-        instance
-            .post(`/user`, { userId: socketId })
-            .then(res => {
-                // 내가방장
-                // isCreator true
-                // spring에 내가 방장임을 알리기
-                console.log(res);
-            })
-            .catch(err => console.log(err));
-    };
+const deleteUser = (socketId) => {
+  return function (dispatch, getState) {
+    console.log("유저삭제", socketId);
+    instance
+      .post(`/user`, { userId: socketId })
+      .then((res) => {
+        // 내가방장
+        // isCreator true
+        // spring에 내가 방장임을 알리기
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
 };
 
 // 코멘트달기
-const writeComment = contents => {
-    return function (dispatch, getState) {
-        instance
-            .post('/comment', {
-                comment: contents,
-            })
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    };
+const writeComment = (contents) => {
+  return function (dispatch, getState) {
+    instance
+      .post("/comment", {
+        comment: contents,
+      })
+      .catch((err) => console.log(err));
+  };
 };
 
 export default handleActions(
-    {
-        [IS_IN]: (state, action) =>
-            produce(state, draft => {
-                draft.isIn = action.payload.isIn;
-            }),
-    },
-    initialState
+  {
+    [IS_IN]: (state, action) =>
+      produce(state, (draft) => {
+        draft.isIn = action.payload.isIn;
+      }),
+  },
+  initialState
 );
 
 // action creator export
