2000d1cfbd3b52c3823d5e63f76ca358eab02457
hyemin <hmwoo94@gmail.com>
2022-03-26 15:25:06 +0900

    [Edit] 페이지 Layout padding top 추가

--- src/Layout/DefaultLayout.jsx
+++ src/Layout/DefaultLayout.jsx
@@ -1,20 +1,21 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Header, Sidebar } from './index';
+import React from "react";
+import styled from "styled-components";
+import { Header, Sidebar } from "./index";
 
 const DefaultLayout = ({ children }) => {
-    return (
-        <>
-            <Sidebar />
-            {/* <Header /> */}
-            <div>
-                <Wrapper>{children}</Wrapper>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Sidebar />
+      {/* <Header /> */}
+      <div>
+        <Wrapper>{children}</Wrapper>
+      </div>
+    </>
+  );
 };
 export default DefaultLayout;
 
 const Wrapper = styled.div`
-    padding-left: 14.58vw;
+  padding-top: 4vw;
+  padding-left: 14.58vw;
 `;
