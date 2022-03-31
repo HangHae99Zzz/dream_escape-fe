8a7b89821f069acbe0a0f479d78945239caa2d07
hyemin <hmwoo94@gmail.com>
2022-04-01 01:45:14 +0900

    [Edit] 마이크 알럿 창 관련 수정

--- src/Component/Game/ThreeRoom.jsx
+++ src/Component/Game/ThreeRoom.jsx
@@ -1,9 +1,15 @@
 import React, { useState, Suspense } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { Canvas } from "@react-three/fiber";
 import { Html, useProgress, PointerLockControls } from "@react-three/drei";
 import { useSelector } from "react-redux";
 
+import { actionCreator as roomActions } from "../../redux/modules/room";
+import { actionCreator as userActions } from "../../redux/modules/user";
+
+import { SvgHome } from "../../Asset/Icon/sidebar/svg_sidebar";
 import Room from "./Room";
 import WasdControls from "./WasdControls";
 import { Modal, ClueModal, GameEndModal, SurveyModal } from "./Modal";
@@ -11,6 +17,9 @@ import GameUsers from "./UI/GameUsers";
 import { EndingCredit } from "../Main";
 
 const ThreeRoom = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const [quizType, setQuizType] = useState("");
   const [clueType, setClueType] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
@@ -22,14 +31,37 @@ const ThreeRoom = () => {
   const [isFirst, setIsFirst] = useState(false);
 
   const { count, countLimit, chance } = useSelector(({ game }) => game);
+  const { socket } = useSelector(({ socket }) => socket);
 
   function Loader() {
     const { progress } = useProgress();
     return <Html center>{progress} % loaded</Html>;
   }
 
+  const exit = () => {
+    socket.close();
+    dispatch(userActions.isIn(false));
+    dispatch(userActions.isCreator(false));
+    dispatch(roomActions.getRoomInfo(null));
+    navigate("/");
+  };
+
   return (
     <Container>
+      <SvgHome
+        width={37}
+        height={37}
+        style={{
+          position: "absolute",
+          zIndex: "2",
+          top: "54px",
+          left: "50px",
+          cursor: "pointer",
+        }}
+        onClick={exit}
+      >
+        나가기
+      </SvgHome>
       <GameUsers
         gameEnd={gameEnd}
         setGameEnd={setGameEnd}
