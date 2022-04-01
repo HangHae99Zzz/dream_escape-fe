c4fb338d8fc8c66ff11778d78930034a78dc6a32
softwat <ghr95223@gmail.com>
2022-04-01 21:59:01 +0900

    [Feat]대기모달 UX, 게임화면 나가기 버튼 resolve #97, resolve #98

--- src/Asset/icon/game/svg_game.js
+++ src/Asset/icon/game/svg_game.js
@@ -1,6 +1,7 @@
-import { ReactComponent as SvgW } from "./w.svg";
-import { ReactComponent as SvgA } from "./a.svg";
-import { ReactComponent as SvgS } from "./s.svg";
-import { ReactComponent as SvgD } from "./d.svg";
+import { ReactComponent as SvgW } from './w.svg';
+import { ReactComponent as SvgA } from './a.svg';
+import { ReactComponent as SvgS } from './s.svg';
+import { ReactComponent as SvgD } from './d.svg';
+import { ReactComponent as SvgExit } from './Exit.svg';
 
-export { SvgW, SvgA, SvgS, SvgD };
+export { SvgW, SvgA, SvgS, SvgD, SvgExit };
